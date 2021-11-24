a7dda3635b8516d1fbbed824952772ee200429fc
pierrevaut <p.vautherin@hotmail.com>
2021-11-24 20:24:13 +0100

    gnurf..

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import ws_server from './ws/ws_server.js';
 
 const app = express();
-
+const port = 3000;
 
 app.use(express.static('packages/app/build'));
 
@@ -10,8 +10,9 @@ app.get('/', function (req, res) {
   res.sendFile('packages/app/build', 'index.html');
 });
 
-console.log("running on port 3000")
+console.log("running on port " + port)
 
 ws_server()
 
-app.listen(3000);
+app.listen(port);
+
