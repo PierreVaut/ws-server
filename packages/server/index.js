1e362dd45f7d1a7c14b32529ac1cd83e2d61e8fc
pierrevaut <p.vautherin@hotmail.com>
2021-11-25 14:23:11 +0100

    change port

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import ws_server from './ws/ws_server.js';
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(express.static('packages/app/build'));
 
